08a175612ea5017ac12172cefecce39788659738
Vladislav Lukyanuk <vlukyanuk.job@gmail.com>
2021-02-13 16:13:51 +0600

    init

---
+++ app/src/bootstrap.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './style.scss';
+
+ReactDom.render(<div>Hi!</div>, document.getElementById("root"));
---
+++ app/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    devServer: {
+        contentBase: path.join(__dirname, "../"),
+        port: 3001,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(svg|png|gif|jpg)$/,
+                exclude: /fonts/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                }
+            },
+            {
+                test: /\.(ttf|eot|woff|svg|woff2)$/,
+                loader: "file-loader",
+                options: {
+                    name: '[path][name].[ext]',
+                }
+            }
+        ],
+    },
+    resolve: {
+        alias: {
+            // components: path.resolve(__dirname, 'src/components'),
+        },
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ],
+    output: {
+        filename: 'index.js',
+        chunkFilename: '[id].bundle.js',
+        path: path.resolve(__dirname, '../'),
+    },
+};
